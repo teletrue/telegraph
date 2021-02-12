c2c62a6537c5ff5bbc1f28f9c54e99a9cc97eb40
teletrue <78961798+teletrue@users.noreply.github.com>
2021-02-12 16:30:41 +0200

    First one
    Now it is mine script which show an amount of views on Telegram's Telegraph pages.

--- getViews.js
+++ getViews.js
@@ -1,26 +1,42 @@
 // ==UserScript==
-// @name         Downloader
+// @name         Telegraph Views
 // @namespace    https://www.youtube.com/channel/UCuxYn-hgUOe7EZbINy_SP8w
 // @version      0.1
-// @description  downloads videos via savefrom.net
-// @author       MewForest
-// @match        https://www.youtube.com/*
+// @description  Show amount of views on Telegraph pages
+// @author       gcontent
+// @match        https://*.telegra.ph/*
 // @grant        none
 // ==/UserScript==
+
+
 
 (function() {
     'use strict';
     setInterval(setDownloader, 1000);
     function setDownloader() {
         let link = document.getElementById('downloader');
+        let LinkAPI = "https://api.telegra.ph/getViews" + window.location.pathname;
+
+        function Get(LinkAPI){
+        var Httpreq = new XMLHttpRequest(); // a new request
+        Httpreq.open("GET",LinkAPI,false);
+        Httpreq.send(null);
+        return Httpreq.responseText;
+        }
+
+        var json_obj = JSON.parse(Get(LinkAPI));
+        console.log("Количество просмотров: " + json_obj.result.views);
+
         if (!link) {
+            var innerText = LinkAPI
             link = document.createElement('a');
-            link.innerText = "Download";
+            link.innerText = " • " + json_obj.result.views;
             link.setAttribute('target', '_blank');
             link.setAttribute('id', 'downloader');
-            document.getElementById('info-text').appendChild(link)
+            document.getElementsByTagName('address')[0].appendChild(link)
         }
-        let hrefDownload = 'https://www.youtubepp.com/watch' + window.location.search;
-        link.setAttribute('href', hrefDownload);
+        let hrefChannel = "http://bit.ly/gc_YouTube";
+
+        link.setAttribute('href', hrefChannel);
     }
 })();
